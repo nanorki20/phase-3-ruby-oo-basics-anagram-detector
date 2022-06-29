87c07ae70b8cdf7e6f4749f124d064f14bda970b
Hayet Kumasseh <naaakleh@gmail.com>
2022-06-29 13:50:03 -0400

    commit tree cf8353ee5e606ab674644ff2d4283403279c281d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    # attr_accessor :match
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        array.select do |x|
+            x.split("").sort == word.split("").sort
+        end
+    end
+
+end
